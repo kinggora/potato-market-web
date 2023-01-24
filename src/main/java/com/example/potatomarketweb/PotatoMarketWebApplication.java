702b4fc48b34b8e9e0f162bb132b5e59a82bfa91
kinggora <jhha09@ajou.ac.kr>
2023-01-24 21:52:58 +0900

    Initial commit

---
+++ src/main/java/com/example/potatomarketweb/PotatoMarketWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.potatomarketweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoMarketWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotatoMarketWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/potatomarketweb/PotatoMarketWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.potatomarketweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoMarketWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
